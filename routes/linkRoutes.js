87bc54ecd899d5a9afcfb8eb22bbc430613c860f
nirmalraaj0275 <124169316+nirmalraaj0275@users.noreply.github.com>
2025-11-22 17:34:51 +0530

    commit

--- routes/linkRoutes.js
+++ routes/linkRoutes.js
@@ -10,13 +10,13 @@ import {
 
 const router = express.Router();
 
-// HEALTH
+
 router.get("/healthz", healthz);
 
-// API ROUTES
+
 router.post("/api/links", createLink);
 router.get("/api/links", getLinks);
-router.get("/:code", getLinkStats);
+router.get("/code/:code", getLinkStats);
 router.delete("/api/links/:code", deleteLink);
 router.get("/:code", handleRedirect);
 
